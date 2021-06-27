771b66a92ef641797aaae4cb8c16f656c38eb935
Daniel Galvez <dg005@lvc.edu>
2021-06-27 14:02:36 -0400

    Added SMS functionality, updated README

--- README.md
+++ README.md
@@ -3,5 +3,66 @@
 This program will ask the client or a stock they would like to predict over the short term,
 and provide help in deciding whether to buy or sell in the matter of minutes or hours. 
 
-To execute this program, you must be using python 3.6 or above, and Anaconda
-To download Anaconda, visit https://www.anaconda.com/products/individual
+
+To execute this program, you will need the following depencies:
+
+• You must be using python 3.6 or above, and Anaconda
+  To download Anaconda, visit https://www.anaconda.com/products/individual
+
+• You must have yahoo finance installed on your machine. Run "pip install yfinance" if you do not have this installed
+
+• For text messaging, this program uses Twilio. You must download Twilio, create an account to generate a phone number,
+  Authentification code, and SID to hard code into the program. To do all of this, follow the steps:
+  ** NOTE: These steps are taken directly from the Twilio Docs. For more information and troubleshooting, visit 
+           https://www.twilio.com/docs/sms/quickstart/python
+
+    1.) Create a Twilio trial account here: https://www.twilio.com/try-twilio
+        A trial account will only let you send to a single phone number. If you wish to send to more phone numbers, 
+        consider upgrading your account
+
+    2.) Execute the following commands:
+
+        MAC: You will need homebrew installed. If you need to install homebrew, visit https://brew.sh/
+
+            Run the commands:
+                brew tap twilio/brew && brew install twilio
+
+                twilio login
+                **After running this command, enter your account SID and Authentification token found on your Twilio account console
+
+                pip install twilio
+
+        WINDOWS: You will need Node.js, to install visit https://nodejs.org/en/download/
+
+                Run the commands:
+                    PS C:\Windows\system32> Set-ExecutionPolicy Bypass -Scope Process
+
+                    Enter 'Y'
+
+                    npm install twilio-cli -g
+        
+                    twilio login
+                    **After running this command, enter your account SID and Authentification token found on your Twilio account console
+
+                    pip install twilio
+
+
+        LINUX: Before we can install, we need to make sure you have Node.js installed (version 10.12 or above). Even if you already         installed Node yourself, the CLI works best when you install it using nvm. Here's how to get nvm installed on most Linux systems:
+
+            Run the commands:
+                curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+                nvm install --lts
+                nvm use <insert version reported from above>
+
+                **Depending on your distribution, you will need to run one of the following commands:
+                    Debian/Ubuntu: sudo apt-get install libsecret-1-dev
+                    Red Hat-based: sudo yum install libsecret-devel
+                    Arch Linux: sudo pacman -S libsecret
+
+                npm install twilio-cli -g
+
+                twilio login
+                **After running this command, enter your account SID and Authentification token found on your Twilio account console
+
+                pip install twilio
--- market_connection.py
+++ market_connection.py
@@ -4,12 +4,19 @@ import pandas as pd
 import yfinance as yf
 import time
 
+# For sending SMS notifications. To configure, see Readme file
+import os
+from twilio.rest import Client
+# Must create an account via Twilio. Upon account creation, you will be given 
+# an Account SID which is the first parameter, and a token
+client = Client("ACxxxxxxxxxxxxxx", "zzzzzzzzzzzzz")
+
 # Ask the client what stock they want to perform this algorithm on
 stock = input("Enter a stock ticker you would like to predict:")
 
 # These will take some fine tuning to get right and therefore should be immutable
 period = '30m'
-interval = '1m'
+interval = '5m'
 
 # Start up the program
 while True:
@@ -19,26 +26,45 @@ while True:
 
     # Calculate the percent change over the specified interval
     perc_change = (((data['Adj Close'][-1] - data['Adj Close'][0]))/data['Adj Close'][0]) * 100
+    print("30 minutes ago,", stock,"was:", data['Adj Close'][0])
+    print("Currently,", stock,"is:", data['Adj Close'][-1])
+
 
     # If the percent change is a DECREASE and less than 3% over the specified short term interval, we should 
     # consider selling. Before pulling the trigger on selling, lets put this into a Long Short Term Memory
     # machine learning model to predict an outcome. If the outcome is unfavorable, SELL. If the prediction 
     # shows that it may rebound, lets HOLD
-    if (perc_change < -3):
+    if (perc_change < 0):
         print (stock, "has incurred a percent decrease of ", perc_change, "percent over the course of", period)
 
         # Implement Long Short Term Memory algorithm
         print("Put in LSTM model to determine whether we should sell")
 
+        # change the "from_" number to your Twilio number and the "to" number
+        # to the phone number you signed up for Twilio with, or upgrade your
+        # account to send SMS to any phone number
+        client.messages.create(to="+13157508492", 
+                            from_="+12532013628", 
+                            body="----NOTICE FROM STOCK PREDICTION AI---- \n Your stock " + str(stock) + " has decreased" + str(perc_change) + " percent in the past " + str(period) + " and is predicted to keep falling. Consider SELLING now.")
+
+
+
     # If the percent change is an INCREASE and 5% or more over the specified short term interval, we should 
     # consider buying. Before pulling the trigger on buying, lets put this into a Long Short Term Memory
     # machine learning model to predict an outcome. If the outcome is favorable, BUY. If the prediction 
     # shows that it may trend back down, ignore
-    elif(perc_change >= 5):
+    elif(perc_change >= 0):
         print (stock, "has incurred a percent increse of ", perc_change, "percent over the course of", period)
 
         # Implement Long Short Term Memory algorithm
         print("Put in LSTM model to determine whether we should buy")
+
+        # change the "from_" number to your Twilio number and the "to" number
+        # to the phone number you signed up for Twilio with, or upgrade your
+        # account to send SMS to any phone number
+        client.messages.create(to="+13157508492", 
+                            from_="+12532013628", 
+                            body="----NOTICE FROM STOCK PREDICTION AI---- \n Your stock " + str(stock) + " has increased " + str(perc_change) + " percent in the past " + str(period) + " and is predicted to keep rising. Consider BUYING now.")
 
 
     else:
