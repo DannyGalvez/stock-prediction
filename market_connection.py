85fbcb36686b3a5f6f1e6b2c232bd193f4392a23
Daniel Galvez <dg005@lvc.edu>
2021-06-29 18:08:09 -0400

    implemented multithreading

--- market_connection.py
+++ market_connection.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 import time
+from pred_stock import pred_stock
 
 # For sending SMS notifications. To configure, see Readme file
 import os
@@ -21,77 +22,16 @@ f.close()
 
 print(SID, token, deliver_number, sender_number)
 
-# Must create an account via Twilio. Upon account creation, you will be given 
-# an Account SID which is the first parameter, and a token
-client = Client(SID, token)
+# Read in stock tickers from a text file. Also added this file to the gitignore. Structure this file such that each 
+# line contains a stock ticker, in all caps, with no added characters or spaces. A pred_stock object will be created 
+# for each stock ticker in the file and a thread will be launched to predict the data.
+with open('my_stock_tickers.txt') as openfileobject:
+    for line in openfileobject:
+        start_thread = pred_stock(SID, token, deliver_number, sender_number, str(line))
+        start_thread.start()
+        print('Launched thread for ' + str(line))
 
-
-# Ask the client what stock they want to perform this algorithm on
-stock = input("Enter a stock ticker you would like to predict:")
-
-# These will take some fine tuning to get right and therefore should be immutable
-period = '30m'
-interval = '5m'
-
-# Start up the program
-while True:
-
-    # Pull stock data from market
-    data = yf.download(tickers= stock, period= period, interval= interval)
-
-    # Calculate the percent change over the specified interval
-    perc_change = (((data['Adj Close'][-1] - data['Adj Close'][0]))/data['Adj Close'][0]) * 100
-    print("30 minutes ago,", stock,"was:", data['Adj Close'][0])
-    print("Currently,", stock,"is:", data['Adj Close'][-1])
-
-
-    # If the percent change is a DECREASE and less than 3% over the specified short term interval, we should 
-    # consider selling. Before pulling the trigger on selling, lets put this into a Long Short Term Memory
-    # machine learning model to predict an outcome. If the outcome is unfavorable, SELL. If the prediction 
-    # shows that it may rebound, lets HOLD
-    if (perc_change < 0):
-        print (stock, "has incurred a percent decrease of ", perc_change, "percent over the course of", period)
-
-        # Implement Long Short Term Memory algorithm
-        print("Put in LSTM model to determine whether we should sell")
-
-        # change the "from_" number to your Twilio number and the "to" number
-        # to the phone number you signed up for Twilio with, or upgrade your
-        # account to send SMS to any phone number
-        client.messages.create(to = deliver_number, 
-                            from_ = sender_number, 
-                            body = "----NOTICE FROM STOCK PREDICTION AI---- \n Your stock " + str(stock) + " has decreased" + str(perc_change) + " percent in the past " + str(period) + " and is predicted to keep falling. Consider SELLING now.")
-
-        time.sleep(900)
-        continue
-
-    # If the percent change is an INCREASE and 5% or more over the specified short term interval, we should 
-    # consider buying. Before pulling the trigger on buying, lets put this into a Long Short Term Memory
-    # machine learning model to predict an outcome. If the outcome is favorable, BUY. If the prediction 
-    # shows that it may trend back down, ignore
-    elif(perc_change >= 0):
-        print (stock, "has incurred a percent increase of ", perc_change, "percent over the course of", period)
-
-        # Implement Long Short Term Memory algorithm
-        print("Put in LSTM model to determine whether we should buy")
-
-        # change the "from_" number to your Twilio number and the "to" number
-        # to the phone number you signed up for Twilio with, or upgrade your
-        # account to send SMS to any phone number
-        client.messages.create(to = deliver_number, 
-                            from_ = sender_number, 
-                            body = "----NOTICE FROM STOCK PREDICTION AI---- \n Your stock " + str(stock) + " has increased " + str(perc_change) + " percent in the past " + str(period) + " and is predicted to keep rising. Consider BUYING now.")
-
-        time.sleep(900)
-        continue
-
-    else:
-        print(perc_change)
-        print("No notable changes. Will try again in two minutes. Going to sleep.....")
-    # Execute the program on two minute intervals
-    time.sleep(120)
-
-
+    
 
 
 
---
+++ pred_stock.py
@@ -0,0 +1,93 @@
+import yfinance as yf
+import time
+import threading
+# For sending SMS notifications. To configure, see Readme file
+import os
+from twilio.rest import Client
+
+# Launch a new thread for each stock
+class pred_stock(threading.Thread):
+    
+    # Constructor, takes in all necessary information from base class
+    def __init__(self, SID, token, deliver_to, send_from, stock):
+        threading.Thread.__init__(self)
+        self.SID = SID
+        self.token = token
+        self.deliver_to = deliver_to
+        self.send_from = send_from
+        self.stock = stock
+
+    # Launch the thread
+    def run(self):
+        SID = self.SID
+        token = self.token
+        deliver_to = self.deliver_to
+        send_from = self.send_from
+        stock = self.stock
+
+        client = Client(self.SID, self.token)
+
+        # These will take some fine tuning to get right and therefore should be immutable
+        period = '30m'
+        interval = '1m'
+
+        # Start up the program
+        while True:
+
+            # Pull stock data from market
+            data = yf.download(tickers= stock, period= period, interval= interval)
+
+            # Calculate the percent change over the specified interval
+            perc_change = (((data['Adj Close'][-1] - data['Adj Close'][0]))/data['Adj Close'][0]) * 100
+            print("30 minutes ago,", stock,"was:", data['Adj Close'][0])
+            print("Currently,", stock,"is:", data['Adj Close'][-1])
+            print("The percent change is:", perc_change)
+
+
+
+            # If the percent change is a DECREASE and less than 3% over the specified short term interval, we should 
+            # consider selling. Before pulling the trigger on selling, lets put this into a Long Short Term Memory
+            # machine learning model to predict an outcome. If the outcome is unfavorable, SELL. If the prediction 
+            # shows that it may rebound, lets HOLD
+            if (perc_change < -2):
+                print (stock, "has incurred a percent decrease of ", perc_change, "percent over the course of", period)
+
+                # Implement Long Short Term Memory algorithm
+                print("Put in LSTM model to determine whether we should sell")
+
+                # Change the "from_" number to your Twilio number and the "to" number
+                # to the phone number you signed up for Twilio with, or upgrade your
+                # account to send SMS to any phone number
+                client.messages.create(to = deliver_to, 
+                                    from_ = send_from, 
+                                    body = "----SELL NOTICE FROM STOCK AI---- \n Your stock " + str(stock) + " has decreased" + str(perc_change) + " percent in the past " + str(period) + " and is predicted to keep falling. Consider SELLING now.")
+
+                print("Going to sleep for 15 minutes...")
+                time.sleep(900)
+                continue
+
+            # If the percent change is an INCREASE and 5% or more over the specified short term interval, we should 
+            # consider buying. Before pulling the trigger on buying, lets put this into a Long Short Term Memory
+            # machine learning model to predict an outcome. If the outcome is favorable, BUY. If the prediction 
+            # shows that it may trend back down, ignore
+            elif(perc_change >= 3):
+                print (stock, "has incurred a percent increase of ", perc_change, "percent over the course of", period)
+
+                # Implement Long Short Term Memory algorithm
+                print("Put in LSTM model to determine whether we should buy")
+
+                # Change the "from_" number to your Twilio number and the "to" number
+                # to the phone number you signed up for Twilio with, or upgrade your
+                # account to send SMS to any phone number
+                client.messages.create(to = deliver_to, 
+                                    from_ = send_from, 
+                                    body = "----BUY NOTICE FROM STOCK AI---- \n Your stock " + str(stock) + " has increased " + str(perc_change) + " percent in the past " + str(period) + " and is predicted to keep rising. Consider BUYING now.")
+            
+                print("Going to sleep for 15 minutes...")
+                time.sleep(900)
+                continue
+
+            else:
+                print("No notable changes. Will try again in two minutes. Going to sleep.....")
+            # Execute the program on two minute intervals
+            time.sleep(120)
